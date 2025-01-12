61adbf342ad269462964629160bb436ab1ad5ed5
Samiksha <agarwalsamiksha88@gmail.com>
2025-01-12 16:49:40 +0530

    Add the function to start and stop colouring

--- script.js
+++ script.js
@@ -1,11 +1,39 @@
-createPixels();
-function createPixels(){
-    const cols = Math.ceil(window.innerHeight / 16), rows = Math.ceil(window.innerWidth / 16);
-    let i, element = document.querySelector('.container');
-    for(i = 0; i < rows * cols; i++){
-        let newElement = document.createElement('div');
-        newElement.classList.add('pixel');
-        element.appendChild(newElement);
+let isActive = false;
+    const container = document.querySelector('.container');
+
+    function createPixels() {
+      const cols = Math.ceil(window.innerWidth / 16);
+      const rows = Math.ceil(window.innerHeight / 16);
+
+      //container.innerHTML = ''; // Clear existing pixels
+
+      for (let i = 0; i < rows * cols; i++) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
+        container.appendChild(pixel);
+      }
     }
-    document.body.appendChild(element);
-}
+
+    // Function that handles mouseenter event
+    function colorPixel(event) {
+      event.target.style.backgroundColor = 'black';
+    }
+
+    // Add event listener for key press
+    document.body.addEventListener("keydown", () => {
+      if (isActive === false) {
+        // Add the mouseenter event listener to all pixels
+        document.querySelectorAll('.pixel').forEach((item) => {
+          item.addEventListener('mouseenter', colorPixel);
+        });
+        isActive = true;
+      } else {
+        // Remove the mouseenter event listener from all pixels
+        document.querySelectorAll('.pixel').forEach((item) => {
+          item.removeEventListener('mouseenter', colorPixel);
+        });
+        isActive = false;
+      }
+    });
+
+    createPixels(); 
